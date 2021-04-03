f75d64f9b55d386733dbc263db595515e0bedb0b
shubham7413 <shubhamofficial7413@gmail.com>
2021-04-04 00:56:45 +0530

    add session for status, wrong ans of time finish will end game

--- index.php
+++ index.php
@@ -8,9 +8,31 @@
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <script>
+        function game_start() {
+            console.log('start_game_executed');
+            document.getElementById('game-start').style.display = 'none';
+            <?php
+            if(session_status() === PHP_SESSION_NONE )
+                session_start();
+            $_SESSION['status_session'] = '1';
+            ?>
+        }
+    </script>
 </head>
 
 <body>
+<script>
+        function game_start() {
+            console.log('start_game_executed');
+            document.getElementById('game-start').style.display = 'none';
+            <?php
+            if(session_status() === PHP_SESSION_NONE )
+                session_start();
+            $_SESSION['status_session'] = '1';
+            ?>
+        }
+    </script>
     <div class="main">
         <div class="logo-container row">
             <div class="logo">
@@ -43,112 +65,71 @@
             </div>
         </div>
     </div>
-    <div class="game-end-container">
+    <div id="game-end" class="game-end-container">
         <div class="details-container">
+            <p id="game-end-detail"></p>
             <p id="score">AAPNE JITA: 10,000</p>
             <div class="play-again-container">
                 <button>Play Again</button>
             </div>
         </div>
     </div>
-    <div class="game-start-container">
+    <div id="game-start" class="game-start-container">
         <div class="start-container">
             <p id="kbc">KAUN BANEGA CROREPATI</p>
             <div class="play-container">
-                <button>Play</button>
+                <button onclick="game_start()">Play</button>
             </div>
         </div>
     </div>
-    <!-- <script> -->
+    <?php
+    if (isset($_SESSION['status_session']) == '1') {
+        include_once('connectvars.php');
+        $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
+            or die('Failed to connect to server');
 
-    <!-- // function countdown_timer(t)
-        // {
-        //     if( t == 60 || t == 45)
-        //     {
-        //         var time_left = t;
-        //         var timer = setInterval(function(){
-
-        //             if (time_left <= 0) {
-        //                 status = 0;
-        //                 clearInterval(timer);
-        //             }
-        //             time_left -= 1;
-        //             document.getElementById("time").innerHTML = time_left;
-        //         },1000)
-        //     }
-        // }
-
-        // var ques_arr = [];
-        // var status = 1;
-        // for(var i = 1; i <= 1; i++)
-        // {
-            
-            // if (status == 1) {
-                
-                //code for timer
-                // if (i <= 5)
-                //     var total_time = 45;
-                // else if(i > 5 && i <= 10)
-                //     var total_time = 60;
-                // else
-                //     var total_time = null;
-                    
-                // countdown_timer(total_time);
-                
-                var total_ques = 1;
-                // var ques_no = parseInt(((Math.random()) * total_ques) + 1);
-
-                // while (!(ques_arr.indexOf(ques_no))) {
-                //     ques_no = parseInt(((Math.random()) * total_ques) + 1);
-                // }
-                // remove below line
-            //     var ques_no = total_ques;
-            //     console.log(ques_no);
-            //     window.location.href = '?id=' + ques_no;
-            // } 
-        // }        -->
-    <!-- </script> -->
-<?php
-    include_once('connectvars.php');
-    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
-        or die('Failed to connect to server');
-
-    $total_ques = 6;
-    $ques_no_php = (int)((rand(1, $total_ques)));
+        $total_ques = 6;
+        $ques_no_php = (int)((rand(1, $total_ques)));
 
 
-    $query = "SELECT * FROM quiz WHERE id =" . $ques_no_php . " ";
-    $result = mysqli_query($dbc, $query)
-        or die('error in quering dB');
-    while ($row = mysqli_fetch_array($result)) {
-        setcookie('ques', $row['ques']);
-        setcookie('option_1', $row['option_1']);
-        setcookie('option_2', $row['option_2']);
-        setcookie('option_3', $row['option_3']);
-        setcookie('option_4', $row['option_4']);
-        setcookie('ans', $row['ans']);
-        break;
+        $query = "SELECT * FROM quiz WHERE id =" . $ques_no_php . " ";
+        $result = mysqli_query($dbc, $query)
+            or die('error in quering dB');
+        while ($row = mysqli_fetch_array($result)) {
+            setcookie('ques', $row['ques']);
+            setcookie('option_1', $row['option_1']);
+            setcookie('option_2', $row['option_2']);
+            setcookie('option_3', $row['option_3']);
+            setcookie('option_4', $row['option_4']);
+            setcookie('ans', $row['ans']);
+            break;
+        }
     }
 
-?>
-    <audio hidden autoplay>
+    ?>
+
+    <audio hidden id="ques_presenting_audio">
         <source src="audio/kbc_ ques.mp3" type="audio/mpeg">
     </audio>
     <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
     <script>
-        var ques = Cookies.get('ques');
-        var option_1 = Cookies.get('option_1');
-        var option_2 = Cookies.get('option_2');
-        var option_3 = Cookies.get('option_3');
-        var option_4 = Cookies.get('option_4');
-        var ans = Cookies.get('ans');
+        if ((<?php echo $_SESSION['status_session']; ?>) == 1) {
+            var ques = Cookies.get('ques');
+            var option_1 = Cookies.get('option_1');
+            var option_2 = Cookies.get('option_2');
+            var option_3 = Cookies.get('option_3');
+            var option_4 = Cookies.get('option_4');
+            var ans = Cookies.get('ans');
 
-        document.getElementById('ques').innerHTML = ques;
-        document.getElementById('option-1').innerHTML = option_1;
-        document.getElementById('option-2').innerHTML = option_2;
-        document.getElementById('option-3').innerHTML = option_3;
-        document.getElementById('option-4').innerHTML = option_3;
-        console.log(ans);
+            document.getElementById('ques').innerHTML = ques;
+            document.getElementById('option-1').innerHTML = option_1;
+            document.getElementById('option-2').innerHTML = option_2;
+            document.getElementById('option-3').innerHTML = option_3;
+            document.getElementById('option-4').innerHTML = option_3;
+
+            document.getElementById('ques_presenting_audio').play();
+        }
+
 
         function output(id, value) {
             document.getElementById(id).innerHTML = value;
@@ -163,13 +144,23 @@
                     output('time', time_left);
                     if (time_left <= 0) {
                         status = 0;
-                        if(time_left < 0)
+                        wrong("Time's Up");
+                        if (time_left < 0)
                             output('time', '0')
                         clearInterval(timer);
                     }
 
                 }, 1000)
             }
+        }
+
+        function correct(){
+
+        }
+        function wrong(detail) {
+            document.getElementById('game-end-detail').innerHTML = detail;
+            document.getElementById('game-end-detail').style.color = 'red';
+            document.getElementById('game-end').style.display = 'block';
         }
 
         function check_ans(option_no) {
@@ -179,13 +170,10 @@
                 document.getElementById(option).style.backgroundColor = 'Green';
             } else {
                 document.getElementById(option).style.backgroundColor = 'Red';
+                wrong('Wrong Ans');
             }
         }
 
-        function game_end()
-        {
-
-        }
         var status = 1;
         if (status == 1) {
 
